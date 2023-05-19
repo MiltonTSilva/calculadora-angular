fdc7e47568692fb57501ec55a0f2a9628a4bc7c8
Milton Tomé da Silva <miltontsilva@gmail.com>
2023-05-18 21:46:49 -0300

    Retirado botão ponto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,16 +23,38 @@ export class AppComponent {
       this.operador == 'X' ||
       this.operador == '/'
     ) {
+      console.log('numero: valor 1 if');
+      console.log('########################');
+      console.log('value: ' + value);
+      console.log('valor: ' + this.valor);
+      console.log('valor1: ' + this.valor1);
+      console.log('valor2: ' + this.valor2);
       this.valor2 += value;
       if (this.valor.includes(this.operador)) {
-        this.valor = this.valor + this.valor2;
+        if (!this.valor.includes(this.pontuacao)) {
+          this.valor = this.valor + this.valor2;
+        } else {
+          this.valor = this.valor1 + this.operador + this.valor2;
+        }
       } else {
         this.valor = this.valor1 + this.operador + this.valor2;
       }
     } else {
-      this.valor1 += value;
-      console.log(value);
+      console.log('numero: valor 2 else');
+      console.log('--------------------------------');
 
+      if (!this.valor.includes(this.pontuacao)) {
+        this.valor1 += this.pontuacao + value;
+      } else {
+        this.valor1 += value;
+      }
+
+      console.log('value: ' + value);
+      console.log('valor: ' + this.valor);
+      console.log('valor1: ' + this.valor1);
+      console.log('valor2: ' + this.valor2);
+      console.log('Pontuacao: ' + this.pontuacao);
+      console.log('operador: ' + this.operador);
       this.valor = this.valor1 + this.operador;
     }
     //console.log(this.valor1);
@@ -41,26 +63,66 @@ export class AppComponent {
   operacao(value: string) {
     this.operador = value;
     if (this.valor2 == '') {
+      //&& this.valor1.substring(0, 3) == '.'
+      console.log('valor1.length: ' + this.valor1.length);
+
+      /*  if (this.valor1.includes('.') && this.valor1.length <= 6) {
+        this.valor1 = this.valor1.replace('.', ',');
+      } */
       this.valor = this.valor1 + this.operador;
     }
   }
   setPontuacao(value: string) {
-    this.pontuacao = value;
-    if (this.valor2 == '') {
-      this.valor1 += this.pontuacao;
-    } else {
-      this.valor2 += this.pontuacao;
-    }
+    console.log('setPontuacao: valor = ' + this.valor);
+    if (
+      (this.valor.length > 0 && !this.valor.includes(value)) ||
+      this.valor.includes(this.operador)
+    ) {
+      this.pontuacao = value;
 
-    this.valor = this.valor1 + this.operador + this.valor2;
+      if (this.valor2 == '' && this.valor1.includes(this.operador)) {
+        this.valor1 += this.pontuacao;
+        console.log('setPontuacao: valor 1 if');
+      } else {
+        console.log('setPontuacao: valor 2 else');
+        console.log('*****************************');
+        console.log('setPontuacao: pontuacao = ' + this.pontuacao);
+        console.log('setPontuacao: valor2 =' + this.valor2);
+        if (!this.valor1.includes(this.operador)) {
+          this.valor2 += this.pontuacao;
+        }
+      }
+      /*  if (this.valor.includes(this.operador)) {
+      this.valor = this.valor + this.valor2;
+    } else { */
+      this.valor = this.valor1 + this.operador + this.valor2;
+    }
+    /* if (this.valor1.includes('.') && this.valor1.length <= 6) {
+      this.valor1 = this.valor1.replace('.', ',');
+      this.pontuacao = '';
+    }
+    if (this.valor2.includes('.') && this.valor2.length <= 6) {
+      this.valor2 = this.valor2.replace('.', ',');
+      this.pontuacao = '';
+    }
+    if (this.valor.includes('.') && this.valor.length <= 6) {
+      this.valor = this.valor.replace('.', ',');
+      this.pontuacao = '';
+    } */
+
+    //}
   }
 
   resultado(value: string) {
     if (value == '=') {
       let soma: number = 0;
-
+      console.log('valor1: ' + this.valor1);
+      console.log('valor2: ' + this.valor2);
       this.valor1 = this.formatarValor(this.valor1).toString();
       this.valor2 = this.formatarValor(this.valor2).toString();
+
+      console.log('valor1: ' + this.valor1);
+      console.log('valor2: ' + this.valor2);
 
       if (this.operador == '+') {
         soma = parseFloat(this.valor1) + parseFloat(this.valor2);
@@ -80,7 +142,7 @@ export class AppComponent {
   }
 
   formatarValor(value: string) {
-    value = value.replace('.', '');
+    //value = value.replace('.', '');
     value = value.replace(',', '.');
     return parseFloat(value);
   }
